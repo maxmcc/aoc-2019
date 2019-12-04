bd98faee3753e71d6856c451aad6139ac0b153b3
Max McCarthy <max_mccarthy@apple.com>
2019-12-04 10:48:49 -0800

    Better solution using iterator adapters

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,5 @@
 #![feature(is_sorted)]
 
-use std::collections::HashMap;
-use std::hash::Hash;
 use std::ops::RangeInclusive;
 
 const INPUT_RANGE: RangeInclusive<usize> = 272091..=815432;
@@ -12,108 +10,38 @@ fn main() {
 }
 
 fn part1() {
-    let mut count = 0;
-    for x in INPUT_RANGE {
-        if valid_password(x) {
-            count += 1;
-        }
-    }
+    let count = INPUT_RANGE
+        .filter(|password| check(*password, |group_len| group_len > 1))
+        .count();
     println!("{}", count);
 }
 
 fn part2() {
-    let mut count = 0;
-    for x in INPUT_RANGE {
-        if valid_password2(x) {
-            count += 1;
-        }
-    }
+    let count = INPUT_RANGE
+        .filter(|password| check(*password, |group_len| group_len == 2))
+        .count();
     println!("{}", count);
 }
 
-trait Frequencies {
-    type Item: Hash + Eq;
-    fn frequencies(self) -> HashMap<Self::Item, usize>;
+fn check(password: usize, group_len_match: impl Fn(usize) -> bool) -> bool {
+    let digits = password.to_string().chars().collect::<Vec<_>>();
+    digits.is_sorted() && groups(&digits).map(|x| x.len()).any(group_len_match)
 }
 
-impl<I: Iterator> Frequencies for I
-where
-    I::Item: Hash + Eq,
-{
-    type Item = I::Item;
-
-    fn frequencies(self) -> HashMap<Self::Item, usize> {
-        let mut map = HashMap::new();
-        for item in self {
-            let counter = map.entry(item).or_insert(0);
-            *counter += 1;
+fn groups<T: PartialEq>(data: &[T]) -> impl Iterator<Item = &[T]> {
+    let mut slice_start = 0;
+    (1..data.len() + 1).flat_map(move |i| {
+        if i == data.len() || data[i - 1] != data[i] {
+            let begin = slice_start;
+            slice_start = i;
+            Some(&data[begin..i])
+        } else {
+            None
         }
-        map
-    }
-}
-
-fn digits(input: usize) -> Vec<usize> {
-    let mut result = Vec::<usize>::new();
-    let mut input = input;
-    while input > 10 {
-        result.push(input % 10);
-        input /= 10;
-    }
-    result.push(input);
-    result.reverse();
-    result
-}
-
-fn valid_password(password: usize) -> bool {
-    if !INPUT_RANGE.contains(&password) {
-        return false;
-    }
-    let digits = digits(password).to_vec();
-    if !digits.iter().is_sorted() {
-        return false;
-    }
-    for window in digits.windows(2) {
-        if window[0] == window[1] {
-            return true;
-        }
-    }
-    false
-}
-
-fn valid_password2(password: usize) -> bool {
-    if !INPUT_RANGE.contains(&password) {
-        return false;
-    }
-    let digits = digits(password).to_vec();
-    if !digits.iter().is_sorted() {
-        return false;
-    }
-    let counts = digits.iter().frequencies();
-    counts.values().any(|x| *x == 2)
+    })
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_frequencies() {
-        let counts = [1, 2, 2, 3, 3, 3].iter().frequencies();
-        assert_eq!(counts.get(&0), None);
-        assert_eq!(counts[&1], 1);
-        assert_eq!(counts[&2], 2);
-        assert_eq!(counts[&3], 3);
-    }
-
-    #[test]
-    fn test_digits() {
-        assert_eq!(digits(123456), [1, 2, 3, 4, 5, 6]);
-    }
-
-    #[test]
-    fn test_valid_password() {
-        assert!(valid_password(333333));
-        assert!(!valid_password(445670));
-        assert!(!valid_password(345678));
-    }
 }
