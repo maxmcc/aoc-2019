0703ad7d7475d4ef9a1674c046128e67f6859298
Max McCarthy <max_mccarthy@apple.com>
2019-12-05 00:40:47 -0800

    Make day 4 solution prettier and faster

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -11,37 +11,110 @@ fn main() {
 
 fn part1() {
     let count = INPUT_RANGE
-        .filter(|password| check(*password, |group_len| group_len > 1))
+        .map(Digits::new)
+        .filter(|digits| {
+            digits.is_sorted()
+                && digits
+                    .collect::<Vec<_>>()
+                    .groups()
+                    .any(|group| group.len() > 1)
+        })
         .count();
     println!("{}", count);
 }
 
 fn part2() {
     let count = INPUT_RANGE
-        .filter(|password| check(*password, |group_len| group_len == 2))
+        .map(Digits::new)
+        .filter(|digits| {
+            digits.is_sorted()
+                && digits
+                    .collect::<Vec<_>>()
+                    .groups()
+                    .any(|group| group.len() == 2)
+        })
         .count();
     println!("{}", count);
 }
 
-fn check(password: usize, group_len_match: impl Fn(usize) -> bool) -> bool {
-    let digits = password.to_string().chars().collect::<Vec<_>>();
-    digits.is_sorted() && groups(&digits).map(|x| x.len()).any(group_len_match)
+#[derive(Clone, Copy, Debug)]
+struct Digits {
+    n: usize,
+    divisor: usize,
 }
 
-fn groups<T: PartialEq>(data: &[T]) -> impl Iterator<Item = &[T]> {
-    let mut slice_start = 0;
-    (1..data.len() + 1).flat_map(move |i| {
-        if i == data.len() || data[i - 1] != data[i] {
-            let begin = slice_start;
-            slice_start = i;
-            Some(&data[begin..i])
+impl Digits {
+    fn new(n: usize) -> Self {
+        let mut divisor = 1;
+        while n >= divisor * 10 {
+            divisor *= 10;
+        }
+        Digits {
+            n: n,
+            divisor: divisor,
+        }
+    }
+}
+
+impl Iterator for Digits {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.divisor == 0 {
+            None
+        } else {
+            let v = self.n / self.divisor;
+            self.n %= self.divisor;
+            self.divisor /= 10;
+            Some(v as u8)
+        }
+    }
+}
+
+struct Groups<'a, T> {
+    base: &'a [T],
+}
+
+impl<'a, T: PartialEq> Iterator for Groups<'a, T> {
+    type Item = &'a [T];
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(item) = self.base.first() {
+            let mut index = 1;
+            while index < self.base.len() && self.base[index] == *item {
+                index += 1;
+            }
+            let (group, rest) = self.base.split_at(index);
+            self.base = rest;
+            Some(group)
         } else {
             None
         }
-    })
+    }
+}
+
+trait GroupsImpl<T> {
+    fn groups(&self) -> Groups<T>;
+}
+
+impl<T> GroupsImpl<T> for [T] {
+    fn groups(&self) -> Groups<T> {
+        Groups { base: self }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_groups() {
+        let groups = [1, 1, 1, 1].groups().map(|g| g.to_vec());
+        assert!(groups.eq(vec![vec![1, 1, 1, 1]]));
+
+        let groups = [1, 1, 2, 3, 2, 2].groups().map(|g| g.to_vec());
+        assert!(groups.eq(vec![vec![1, 1], vec![2], vec![3], vec![2, 2]]));
+
+        let groups = [].groups().map(|g: &[i32]| g.to_vec());
+        assert_eq!(groups.count(), 0);
+    }
 }
